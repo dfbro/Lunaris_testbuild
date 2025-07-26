334b2eaeeee2b24ccef11d9eba09cd91f7743a2a
vlctyy <kharispowell@hotmail.com>
2025-07-26 12:18:43 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,39 +3,39 @@ plugins {
 }
 
 android {
-    namespace = "com.zenostrap.rbx" // shit’s renamed, don’t forget the manifest
+    namespace = "com.lunaris.rbx" // keep in sync with manifest
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.zenostrap.rbx" // idk just keep this in sync
+        applicationId = "com.lunaris.rbx"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2 // bumped it, don’t ship 1.0 twice
-        versionName = "2.0" // yeah
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true // shrink that fat release build
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
         debug {
-            isMinifyEnabled = false // no one’s debugging obfuscated bs
+            isMinifyEnabled = false
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11 // don't touch unless u know wtf you're doing
+        sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
     buildFeatures {
-        viewBinding = true // godsend, don't disable this unless ur high
+        viewBinding = true
     }
 }
 
